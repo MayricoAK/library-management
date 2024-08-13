a87d71adae69e2d1a1fc8b95346e3c47c5170270
MayricoAK <mayricoak@gmail.com>
2024-08-13 20:44:14 +0700

    fix getBorrowedBooksCount and modified Member routes

--- controllers/Member.js
+++ controllers/Member.js
@@ -13,13 +13,13 @@ exports.getAllMembers = async (req, res) => {
 // Get member's borrowed books count
 exports.getBorrowedBooksCount = async (req, res) => {
   try {
-    const { memberId } = req.params;
-    const member = await Member.findById(memberId).populate('borrowedBooks');
+    const { code } = req.params;
+    const member = await Member.findOne({ code: code }).populate('borrowedBooks'); // Ensure the filter is an object
     if (!member) {
       return res.status(404).json({ message: 'Member not found' });
     }
 
-    res.status(200).json({ message: 'Borrowed books count', count: member.borrowedBooks.length });
+    res.status(200).json({ message: 'Borrowed books by member', Total: member.borrowedBooks.length, books: member.borrowedBooks });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- routes/Member.js
+++ routes/Member.js
@@ -6,6 +6,6 @@ const {
 } = require('../controllers/Member');
 
 router.get('/', getAllMembers);
-router.get('/:memberId/borrowed-books-count', getBorrowedBooksCount);
+router.get('/:code', getBorrowedBooksCount);
 
 module.exports = router;
